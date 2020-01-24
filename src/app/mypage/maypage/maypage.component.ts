f6fc0e0a243e05d3324215bee84983f4fdbe6521
hiranumaharuka <colorcord251@gmail.com>
2020-01-24 17:03:14 +0900

    feat(header):ヘッダーを作成しました#2

---
+++ src/app/mypage/maypage/maypage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maypage',
+  templateUrl: './maypage.component.html',
+  styleUrls: ['./maypage.component.scss']
+})
+export class MaypageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
