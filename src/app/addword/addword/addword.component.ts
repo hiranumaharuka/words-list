24c64a4798d3f01da9a0c809f0df2c3dea6206af
hiranumaharuka <colorcord251@gmail.com>
2020-05-14 19:19:43 +0900

    feat(word): 課金による機能制限実装 #20

--- src/app/addword/addword/addword.component.ts
+++ src/app/addword/addword/addword.component.ts
@@ -1,31 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Validators, FormBuilder, FormControl, NgForm } from '@angular/forms';
 import { Word } from 'src/app/interfaces/word';
 import { WordService } from 'src/app/services/word.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { Observable } from 'rxjs';
-import { switchMap, take } from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
+import { switchMap, take, map } from 'rxjs/operators';
+import { VocabularyService } from 'src/app/services/vocabulary.service';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-addword',
   templateUrl: './addword.component.html',
   styleUrls: ['./addword.component.scss']
 })
-export class AddwordComponent implements OnInit {
+export class AddwordComponent implements OnInit, OnDestroy {
   vocabularyId: string;
   form = this.fb.group({
     surface: ['', [Validators.required, Validators.maxLength(300)]],
     backside: ['', [Validators.required, Validators.maxLength(200)]]
   });
   isEditing: boolean;
+  uid = this.authService.uid;
   word$: Observable<Word>;
   word: Word;
   wordId: string;
+  wordsLength$: Observable<number>;
+  wordsLength;
+  isCustomer: boolean;
+  isCustomer$: Observable<boolean> = this.vocabularyService
+    .getUser(this.uid)
+    .pipe(map(data => data.isCustomer));
+  sub: Subscription;
+
   get surfaceControl() {
     return this.form.get('surface') as FormControl;
   }
+
   get backsideControl() {
     return this.form.get('backside') as FormControl;
   }
@@ -35,18 +47,25 @@ export class AddwordComponent implements OnInit {
     private wordService: WordService,
     private authService: AuthService,
     private route: ActivatedRoute,
-    private location: Location
+    private location: Location,
+    private vocabularyService: VocabularyService,
+    private snackBar: MatSnackBar
   ) {}
 
   ngOnInit() {
     this.word$ = this.route.queryParamMap.pipe(
-      switchMap(map => {
-        const wordId = map.get('wordId');
+      switchMap(param => {
+        const wordId = param.get('wordId');
         return this.wordService.getWord(this.vocabularyId, wordId);
       })
     );
     this.vocabularyId = this.route.snapshot.paramMap.get('vocabularyId');
     this.patchDefaultValue();
+    this.wordsLength$ = this.wordService.getWordsLength(this.vocabularyId);
+    this.sub = this.wordsLength$.subscribe(data => {
+      this.wordsLength = data;
+    });
+    this.sub = this.isCustomer$.subscribe(data => (this.isCustomer = data));
   }
 
   submit(form: NgForm) {
@@ -57,11 +76,19 @@ export class AddwordComponent implements OnInit {
       createdAt: new Date(),
       authorId: this.authService.uid
     };
-    this.wordService.addWord(sendData, this.authService.uid, this.vocabularyId);
+
+    this.wordService.addWord(sendData, this.uid, this.vocabularyId);
     form.resetForm();
   }
+
   goBack(): void {
     this.location.back();
+  }
+
+  alert(): void {
+    this.snackBar.open('無料会員の方はこれ以上登録できません', null, {
+      duration: 2000
+    });
   }
 
   updateWord() {
@@ -104,5 +131,9 @@ export class AddwordComponent implements OnInit {
         this.isEditing = false;
       }
     });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
--- src/app/wordlist/wordlist/wordlist.component.ts
+++ src/app/wordlist/wordlist/wordlist.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { VocabularyService } from 'src/app/services/vocabulary.service';
 import { ActivatedRoute } from '@angular/router';
 import { Vocabulary } from 'src/app/interfaces/vocabulary';
-import { Observable } from 'rxjs';
-import { switchMap, take } from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
+import { switchMap, take, map } from 'rxjs/operators';
 import { AuthService } from 'src/app/services/auth.service';
 import { Word } from 'src/app/interfaces/word';
 import { WordService } from 'src/app/services/word.service';
 import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-wordlist',
   templateUrl: './wordlist.component.html',
   styleUrls: ['./wordlist.component.scss']
 })
-export class WordlistComponent implements OnInit {
+export class WordlistComponent implements OnInit, OnDestroy {
   vocabulary$: Observable<Vocabulary>;
   userId: string = this.authService.uid;
   lastDoc;
@@ -30,24 +31,38 @@ export class WordlistComponent implements OnInit {
   flipped: boolean;
   words: Word[] = [];
   progress: number;
+  wordsLength$: Observable<number>;
+  wordsLength;
+  isCustomer: boolean;
+  isCustomer$: Observable<boolean> = this.vocabularyService
+    .getUser(this.userId)
+    .pipe(map(data => data.isCustomer));
+  sub: Subscription;
   constructor(
     private route: ActivatedRoute,
     private vocabularyService: VocabularyService,
     private authService: AuthService,
-    private wordService: WordService
+    private wordService: WordService,
+    private snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
     this.vocabulary$ = this.route.paramMap.pipe(
-      switchMap(map => {
-        const vocabularyId = map.get('vocabularyId');
+      switchMap(param => {
+        const vocabularyId = param.get('vocabularyId');
         return this.vocabularyService.getVocabulary(vocabularyId);
       })
     );
     this.vocabularyId = this.route.snapshot.paramMap.get('vocabularyId');
     this.getWords();
     this.flipped = true;
+    this.wordsLength$ = this.wordService.getWordsLength(this.vocabularyId);
+    this.sub = this.wordsLength$.subscribe(data => {
+      this.wordsLength = data;
+    });
+    this.sub = this.isCustomer$.subscribe(data => (this.isCustomer = data));
   }
+
   getWords() {
     if (this.isComplete) {
       return;
@@ -68,20 +83,34 @@ export class WordlistComponent implements OnInit {
         }
       });
   }
+
   deleteWord(wordId: string) {
     const targetIndex = this.words.findIndex(word => word.wordId === wordId);
     this.words.splice(targetIndex, 1);
   }
+
   flip() {
     this.flipped = false;
   }
+
   list() {
     this.flipped = true;
   }
+
   onIndexChange(index: number) {
     const result = (this.index / (this.words.length - 1)) * 100;
     const n = 0;
     this.progress = Math.floor(result * Math.pow(10, n)) / Math.pow(10, n);
     return index;
+  }
+
+  alert(): void {
+    this.snackBar.open('無料会員の方はこれ以上登録できません', null, {
+      duration: 2000
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
